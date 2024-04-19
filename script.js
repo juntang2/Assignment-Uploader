1b4fcbd3a29ebc1bc34dced363ef14a7ebc97372
juntang2 <149381509+juntang2@users.noreply.github.com>
2024-04-19 08:22:39 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const form = document.querySelector("form"),
+fileInput = document.querySelector(".file-input"),
+progressArea = document.querySelector(".progress-area"),
+uploadedArea = document.querySelector(".uploaded-area");
+
+form.addEventListener("click", () =>{
+  fileInput.click();
+});
+
+fileInput.onchange = ({target})=>{
+  let file = target.files[0]; //files [0] ni kalau user pilih multiple files ambik yang first je
+  if(file){
+    let fileName = file.name; //getting file name
+    if(fileName.length >= 12){ //if file name length is greater than 12 then split it and add ...
+      let splitName = fileName.split('.');
+      fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
+    }
+    uploadFile(fileName);
+  }
+}
+
+
+function uploadFile(name){
+  let xhr = new XMLHttpRequest(); 
+  xhr.open("POST", "php/upload.php"); //sending post request to the specified URL
+  xhr.upload.addEventListener("progress", ({loaded, total}) =>{ //untuk file uploading progress 
+    let fileLoaded = Math.floor((loaded / total) * 100);  //percentage of loaded file size
+    let fileTotal = Math.floor(total / 1000); //nak tahu total file size dalam KB guna bytes
+    let fileSize;
+    // if file size is less than 1024 then add only KB else convert this KB into MB
+    (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024*1024)).toFixed(2) + " MB";
+    let progressHTML = `<li class="row">
+                          <i class="fas fa-file-alt"></i>
+                          <div class="content">
+                            <div class="details">
+                              <span class="name">${name} • Uploading</span>
+                              <span class="percent">${fileLoaded}%</span>
+                            </div>
+                            <div class="progress-bar">
+                              <div class="progress" style="width: ${fileLoaded}%"></div>
+                            </div>
+                          </div>
+                        </li>`;
+    // uploadedArea.innerHTML = ""; //uncomment line ni kalau tak nak tunjuk upload history
+    uploadedArea.classList.add("onprogress");
+    progressArea.innerHTML = progressHTML;
+    if(loaded == total){
+      progressArea.innerHTML = "";
+      let uploadedHTML = `<li class="row">
+                            <div class="content upload">
+                              <i class="fas fa-file-alt"></i>
+                              <div class="details">
+                                <span class="name">${name} • Uploaded</span>
+                                <span class="size">${fileSize}</span>
+                              </div>
+                            </div>
+                            <i class="fas fa-check"></i>
+                          </li>`;
+      uploadedArea.classList.remove("onprogress");
+      // uploadedArea.innerHTML = uploadedHTML; //refer line 44
+      uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML); //remove line ni kalau tak nak tunjuk upload history
+    }
+  });
+  let data = new FormData(form); 
+  xhr.send(data); 
+}
